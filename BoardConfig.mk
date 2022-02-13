e494ee71fcd48f9827343117e324c618dc1340d3
sthomsonpl <tomekciupa07.02@wp.pl>
2022-02-13 02:12:43 +0100

    lemonkebab: SELINUX_IGNORE_NEVERALLOWS=TRUE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -212,7 +212,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-
+SELINUX_IGNORE_NEVERALLOWS := true
 # USB
 TARGET_QTI_USB_SUPPORTS_AUDIO_ACCESSORY := true
 
